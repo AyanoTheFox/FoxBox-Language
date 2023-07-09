d8c628dba5eb77ce31464a39580e7eb700f94657
AyanoTheoxy <138265796+AyanoTheFox@users.noreply.github.com>
2023-07-09 01:44:09 -0300

    Update parser.lua

--- parser.lua
+++ parser.lua
@@ -1,8 +1,9 @@
 require 'Addons/string'
 parser = {}
+buffer = {} --virtual memory
 
 function parser:run(cmd)
-    if not cmd:match('^%s*$') then
+    if not string.isOnly(cmd, '%s') then
         local commentary = string.split(cmd, '?')
         local token = string.split(commentary[1], ';')
         
@@ -28,22 +29,28 @@ function parser:run(cmd)
                         end
                         
                         if string.math(table.concat(expression)) == math.floor(string.math(table.concat(expression))) then
-                            _G[memb[1]:gsub('%s', '')] = string.math(table.concat(expression))
-                            print(_G[memb[1]:gsub('%s', '')])
+                            buffer[memb[1]:gsub('%s', '')] = string.math(table.concat(expression))
+                            --print(buffer[memb[1]:gsub('%s', '')])
                         else
                             error("the expression result not is a int number")
                         end
                     end
                 elseif param[1] == 'float:' then
-                    if string.between(memb[2], '"', '"')[1] then
+                    if string.between(memb[2], '"', '"')[1] or string.between(memb[2], "'", "'") then
                         error("expected number got string")
                     elseif param[2] == 'false' or param[2] == 'true' then
                         error("expected number got bool")
                     elseif not tonumber(param[2]) then
                         error("expected number got nil")
                     else
-                        _G[memb[1]:gsub('%s', '')] = string.math(param[2])
-                        print(_G[memb[1]:gsub('%s', '')])
+                        local expression = {}
+                        
+                        for p = 2, #param do
+                            table.insert(expression, param[p])
+                        end
+                        
+                        buffer[memb[1]:gsub('%s', '')] = string.math(table.concat(expression, ''))
+                        --print(buffer[memb[1]:gsub('%s', '')])
                     end
                 elseif param[1] == 'unsigned' then
                     if param[2] == 'int:' or param[2] == 'float:' then
@@ -65,7 +72,7 @@ function parser:run(cmd)
                             else
                                 error("expected unsigned number got nil")
                             end
-                        elseif tonumber(param[3]) ~= math.floor(tonumber(param[3]))  then
+                        elseif tonumber(param[3]) ~= math.floor(tonumber(param[3])) then
                             if param[2] == 'int:' then
                                 error("expected unsigned int got float")
                             else
@@ -85,8 +92,8 @@ function parser:run(cmd)
                             end
                         
                             if string.math(table.concat(expression)) ~= -math.abs(string.math(table.concat(expression))) or string.math(table.concat(expression)) ~= math.floor(string.math(table.concat(expression))) then
-                                _G[memb[1]:gsub('%s', '')] = string.math(table.concat(expression))
-                                print(_G[memb[1]:gsub('%s', '')])
+                                buffer[memb[1]:gsub('%s', '')] = string.math(table.concat(expression))
+                                --print(buffer[memb[1]:gsub('%s', '')])
                             elseif string.math(table.concat(expression)) ~= -math.abs(string.math(table.concat(expression))) then
                                 error("the expression result in a  not positive number")
                             else
@@ -136,8 +143,8 @@ function parser:run(cmd)
                             end
                         
                             if string.math(table.concat(expression)) ~= math.abs(string.math(table.concat(expression))) then
-                                _G[memb[1]:gsub('%s', '')] = string.math(table.concat(expression))
-                                print(_G[memb[1]:gsub('%s', '')])
+                                buffer[memb[1]:gsub('%s', '')] = string.math(table.concat(expression))
+                                --print(buffer[memb[1]:gsub('%s', '')])
                             elseif string.math(table.concat(expression)) ~= math.abs(string.math(table.concat(expression))) then
                                 error("the expression result not is a negative number")
                             end
@@ -156,7 +163,6 @@ function parser:run(cmd)
                         str = string.between(memb[2], '"', '"')[1] or string.between(memb[2], "'", "'")[1]
                     end
                     
-                    
                     if not param[2] then
                         error("expected string got nil")
                     elseif tonumber(param[2]) then
@@ -164,8 +170,8 @@ function parser:run(cmd)
                     elseif param[2] == 'false' or param[2] == 'true' then
                         error("expected string got bool")
                     else
-                        _G[memb[1]:gsub('%s', '')] = str
-                        print(_G[memb[1]:gsub('%s', '')])
+                        buffer[memb[1]:gsub('%s', '')] = str
+                        --print(buffer[memb[1]:gsub('%s', '')])
                     end
                 elseif param[1] == 'char:' then
                     local char = string.between(memb[2], '"', '"')[1] or string.between(memb[2], "'", "'")[1]
@@ -177,8 +183,8 @@ function parser:run(cmd)
                     elseif param[2] == 'false' or param[2] == 'true' then
                         error("expected char got bool")
                     elseif char:len() == 1 then
-                        _G[memb[1]:gsub('%s', '')] = char
-                        print(_G[memb[1]:gsub('%s', '')])
+                        buffer[memb[1]:gsub('%s', '')] = char
+                        --print(buffer[memb[1]:gsub('%s', '')])
                     end
                 elseif param[1] == 'bool:' then
                     if string.between(memb[2], '"', '"')[1] then
@@ -186,24 +192,26 @@ function parser:run(cmd)
                     elseif not param[2] then
                         error("expected bool got nil")
                     elseif param[2] == 'false' or tonumber(param[2]) == 0 then
-                        _G[memb[1]:gsub('%s', '')] = false
-                        print(_G[memb[1]:gsub('%s', '')])
+                        buffer[memb[1]:gsub('%s', '')] = false
+                        print(buffer[memb[1]:gsub('%s', '')])
                     elseif param[2] == 'true' or tonumber(param[2]) == 1 then
-                        _G[memb[1]:gsub('%s', '')] = true
-                        print(_G[memb[1]:gsub('%s', '')])
+                        buffer[memb[1]:gsub('%s', '')] = true
+                        --print(buffer[memb[1]:gsub('%s', '')])
                     end
                 else
                     local expression = {}
                     
                     for p = 1, #param do
-                        table.insert(expression, _G[param[p]:gsub('%s', '')] or param[p])
+                        table.insert(expression, buffer[param[p]:gsub('%s', '')] or param[p])
                     end
                     
-                    _G[memb[1]:gsub('%s', '')] = string.math(table.concat(expression))
-                    print(_G[memb[1]:gsub('%s', '')])
+                    buffer[memb[1]:gsub('%s', '')] = string.math(table.concat(expression))
+                    --print(buffer[memb[1]:gsub('%s', '')])
                 end
             end
         end
+        
+        return buffer
     end
 end
 
