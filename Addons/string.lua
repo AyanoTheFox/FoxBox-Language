e20cdeee44209115f705769155c79120b493dea2
AyanoTheoxy <138265796+AyanoTheFox@users.noreply.github.com>
2023-07-09 01:45:35 -0300

    Update string.lua

--- Addons/string.lua
+++ Addons/string.lua
@@ -1,7 +1,7 @@
 function string.split(str, split)
     local t = {}
     
-    for substr in string.gmatch(str, '([^' .. (split or 's%') .. ']+)') do
+    for substr in str:gmatch('([^' .. (split or 's%') .. ']+)') do
         table.insert(t, substr)
     end
     
@@ -11,11 +11,17 @@ end
 function string.between(str, between1, between2)
     local t = {}
     
-    for substr in string.gmatch(str, (between1 or '%s') .. '(.+)' .. (between2 or '%s')) do
+    for substr in str:gmatch((between1 or '%s') .. '(.+)' .. (between2 or '%s')) do
         table.insert(t, substr)
     end
     
     return t
+end
+
+function string.isOnly(str, is)
+    if str:match('^' .. is .. '*$') then
+        return true
+    end
 end
 
 function string.math(str)
@@ -24,4 +30,4 @@ function string.math(str)
     return tonumber(func())
 end
 
-return string.split, string.between, string.math
+return string.split, string.between, string.math, string.isOnly
