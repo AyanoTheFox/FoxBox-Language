5e396dccea7a9be03e7f48a25c027aa5c66e1b4b
AyanoTheoxy <138265796+AyanoTheFox@users.noreply.github.com>
2023-07-09 01:48:34 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,20 @@
 --[[
-    fox languague created by devlops ayano and help of prof choco <3 on day 21th juny of 2023 to versions of lua 3.5.6 or bigger than
+    foxbox languague created by devlops ayano and help of prof choco <3 on day 21th juny of 2023 to versions of lua 3.5.6 or bigger than
 ]]
 parser = require 'parser'
+foxbox = {}
 
-local data = io.open('Test.fb')
-    for l in data:lines() do
-        parser:run(l)
+function foxbox.decode(file)
+    if not file then
+        error("userdata expected got nil")
     end
-data:close()
+    
+    local t, data = {}, io.open(file)
+    for l in data:lines() do
+        t = parser:run(l)
+    end
+    data:close()
+    return t
+end
+
+return foxbox
